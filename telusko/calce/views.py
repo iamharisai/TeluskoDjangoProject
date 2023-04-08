fe23890743fb821b91120c08440c23cbff17989a
hariiisai <harisai.58@gmail.com>
2023-04-08 18:40:25 +0530

    Added form in home.html to addition functionality

--- telusko/calce/views.py
+++ telusko/calce/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("<h1>Hello world!</h1>")
     return render(request,'home.html',{'name': 'Harisai'})
+
+def add(request):
+
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    result = val1 + val2
+
+    return render(request, "results.html",{'result': result})  
